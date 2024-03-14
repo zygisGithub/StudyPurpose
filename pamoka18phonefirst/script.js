3fade560af9a96e5e5445936cfc803d748781d25
zygisGithub <161710612+zygisGithub@users.noreply.github.com>
2024-03-14 15:21:16 +0200

    Update script.js

--- pamoka18phonefirst/script.js
+++ pamoka18phonefirst/script.js
@@ -1,1 +1,12 @@
+function showMore() {
+    var moreText = document.getElementById("moreText");
+    var readMoreLink = document.getElementById("readMoreLink");
 
+    if (moreText.style.display === "none") {
+        moreText.style.display = "inline";
+        readMoreLink.textContent = "Read Less";
+    } else {
+        moreText.style.display = "none";
+        readMoreLink.textContent = "Read More...";
+    }
+}
